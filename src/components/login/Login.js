54331a6c54cf58418b99007ead02d31de33b28b4
ismaelg456g <ismael-456@hotmail.com>
2020-10-07 20:21:49 -0300

    [Issue_76] Links form with object

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,22 +1,52 @@
-import React from "react"
-import ReactDOM from 'react-dom';
-import './Login.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./Login.css";
 
-function Login(){
-  return(
-    <div class='login'>
-      <div class="login-form">
-        <form>
-          <img src="logo_blue.png" alt="UnB" class = "logo"/>
-          <input type="text" id="name" class="form-control login-input" placeholder="Nome" />
-          <input type="password" id="password" class="form-control login-input" placeholder="Senha" />
-          <button class="btn btn-block button-next" type="submit">Entrar</button>
-          <p class="registration-text">Não possui conta? <a href="#">Crie</a> agora</p>
-        </form>
+class Login extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event){
+    if(event.target.id === "username"){
+      this.setState({username: event.target.value});
+    }
+    if(event.target.id === "password"){
+      this.setState({password: event.target.value});
+    }
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+
+    const login = this.state;
+
+    console.log(login);
+
+  }
+
+  render(){
+    return(
+      <div className="login">
+        <div className="login-form">
+          <form onSubmit={this.handleSubmit}>
+            <img src="logo_blue.png" alt="UnB" className = "logo"/>
+            <input type="text" id="username" value={this.state.username} onChange={this.handleChange} className="form-control login-input" placeholder="Nome de Usuário" />
+            <input type="password" id="password" value={this.state.password} onChange={this.handleChange} className="form-control login-input" placeholder="Senha" />
+            <button className="btn btn-block button-next"  type="submit">Entrar</button>
+            <p className="registration-text">Não possui conta? <a href="#">Crie</a> agora</p>
+          </form>
+        </div>
       </div>
-    </div>
-
-  );
+    );  
+  }
 }
 
 export default Login;
--- src/components/login/Registration.js
+++ src/components/login/Registration.js
@@ -1,23 +1,63 @@
-import React from "react"
-import ReactDOM from 'react-dom';
-import './Login.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./Login.css";
 
-function Login(){
-  return(
-    <div class='login'>
-      <div class="login-form">
-        <form>
-          <img src="logo_blue.png" alt="UnB" class = "logo"/>
-          <input type="text" id="name" class="form-control login-input" placeholder="Nome" />
-          <input type="email" id="email" class="form-control login-input" placeholder="Email" />
-          <input type="password" id="password" class="form-control login-input" placeholder="Senha" />
-          <button class="btn btn-block button-next" type="submit">Criar</button>
-          <p class="registration-text">Já possui conta? <a href="#">Entre</a> agora</p>
-        </form>
+class Registration extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      name: "",
+      surname: "",
+      username: "",
+      password: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event){
+    if(event.target.id === "name"){
+      this.setState({name: event.target.value});
+    }
+    if(event.target.id === "surname"){
+      this.setState({surname: event.target.value});
+    }
+    if(event.target.id === "username"){
+      this.setState({username: event.target.value});
+    }
+    if(event.target.id === "password"){
+      this.setState({password: event.target.value});
+    }
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+
+    const registration_form = this.state;
+
+    console.log(registration_form);
+
+  }
+
+  render(){
+    return(
+      <div className="login">
+        <div className="login-form">
+          <form onSubmit={this.handleSubmit} >
+            <img src="logo_blue.png" alt="UnB" className = "logo"/>
+            <input type="text" id="name" value={this.state.name} onChange={this.handleChange} className="form-control login-input" placeholder="Nome" />
+            <input type="text" id="surname" value={this.state.surname} onChange={this.handleChange} className="form-control login-input" placeholder="Sobrenome" />
+            <input type="text" id="username" value={this.state.username} onChange={this.handleChange} className="form-control login-input" placeholder="Nome de Usuário" />
+            <input type="password" id="password" value={this.state.password} onChange={this.handleChange} className="form-control login-input" placeholder="Senha" />
+            <button className="btn btn-block button-next" type="submit">Criar</button>
+            <p className="registration-text">Já possui conta? <a href="#">Entre</a> agora</p>
+          </form>
+        </div>
       </div>
-    </div>
 
-  );
+    );
+  }
 }
 
-export default Login;
+export default Registration;
