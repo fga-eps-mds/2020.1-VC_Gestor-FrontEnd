d93d8457cbdcc6362f3389793546b4eaf1c514b2
tomasvelos0 <tomasveloso@gmail.com>
2020-11-11 15:36:10 -0300

    Add links.
    Co-authored-by: julianavalle <juju.pvg@gmail.com>

--- src/components/components/Sidebar.js
+++ src/components/components/Sidebar.js
@@ -1,5 +1,5 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 import { Row, Col } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChartBar, faBell, faTrophy, faBullhorn, faChevronRight, faChevronDown } from "@fortawesome/free-solid-svg-icons";
